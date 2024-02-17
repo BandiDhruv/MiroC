bec82193ac027d24e1e474f8e2416c181fc9b1ff
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-02-17 14:50:14 +0530

    routes for board creating and schema creating

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,6 +1,27 @@
+"use client"
+// import {useMutation} from "convex/react"
+import {api} from "@/convex/_generated/api"
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 export const EmptyBoards = () =>{
+    const { organization }=useOrganization();
+    const {mutate,pending} =useApiMutation(api.board.create);
+    const onClick = () =>{
+        if(!organization)return ;
+        mutate({
+            orgId:organization.id,
+            title:"Untitled"
+        })
+        .then((id)=>{
+            toast.success("Board created");
+            //todo -> redirect to boards/[id]
+        })
+        .catch(()=>toast.error("Failed to create board"));
+
+    }
     return(
         <div className="h-full flex flex-col items-center justify-center">
             <Image 
@@ -16,7 +37,7 @@ export const EmptyBoards = () =>{
                 start by creating a board for your organization
             </p>
             <div className="mt-6">
-                <Button size="lg">
+                <Button disabled={pending} onClick={onClick} size="lg">
                     Create Board
                 </Button>
             </div>
