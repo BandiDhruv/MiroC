999f59dd4a6a12d9afb65e43763b90666923c2b3
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-02-21 17:28:34 +0530

    board list displaying with proper ui

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,8 +1,12 @@
 "use client"
 
+import { useQuery } from "convex/react";
+import {api} from "@/convex/_generated/api"
+import { BoardCard } from "./board-card"
 import { EmptyFavorites } from "./empty-favorite";
 import { EmptySearch } from "./empty-search";
 import {EmptyBoards} from "./empty-boards"
+import { NewBoardButton } from "./new-board-button";
 interface BoardListProps {
     orgId:string;
     query:{
@@ -15,7 +19,29 @@ export const BoardList =({
     orgId,
     query,
 }:BoardListProps) =>{
-    const data=[];
+    const data=useQuery(api.boards.get,{orgId});
+    
+    //id data === NULL than there is no boards
+    if(data===undefined) //means loading state
+    {
+        return(
+            <div>
+            <h2 className="text-3xl">
+                {query.favorites?"Favorites Boards":"Team Boards"}
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+
+            <NewBoardButton orgId={orgId} disabled/>
+            <BoardCard.Skeleton />
+            <BoardCard.Skeleton />
+            <BoardCard.Skeleton />
+            <BoardCard.Skeleton />
+            <BoardCard.Skeleton />
+            </div>
+            </div>
+        )
+    }
+
     if(!data?.length && query.search){
         return <EmptySearch />
     }
@@ -27,7 +53,26 @@ export const BoardList =({
     }
     return(
         <div>
-            {JSON.stringify(query)}
+            <h2 className="text-3xl">
+                {query.favorites?"Favorites Boards":"Team Boards"}
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                <NewBoardButton orgId={orgId}/>
+            {data?.map((board)=>(
+                <BoardCard 
+                    key={board._id}
+                    id={board._id}
+                    title={board.title}
+                    imageUrl={board.imageUrl}
+                    authorId={board.authorId}
+                    authorName={board.authorName}
+                    createdAt= {board._creationTime}
+                    orgId= {board.orgId}
+                    isFavorite={false}
+                />
+            ))}
+            </div>
+            {/* {JSON.stringify(data)} */}
         </div>
     )
 }
