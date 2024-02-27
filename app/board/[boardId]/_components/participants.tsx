f0df759d172a131c802abaf1c55bda10fb5262e8
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-02-27 16:58:57 +0530

    liveblocks setup

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,7 +1,12 @@
 export const Participants = () =>{
     return (
-        <div className="absolute h-12 top-12 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
+        <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
             List Of Users
         </div>
+    )
+}
+Participants.Skeleton = function ParticipantsSkeleton(){
+    return (
+        <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]" />
     )
 }
